c022fd48f1f5d51686d2f562223910bada8fbae2
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-06 06:22:03 +0100

    Add name and parent_permission to super

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Student < Person
   def initialize(age, classroom)
-    super(age)
+    super(age,name = 'Unknown', parent_permission: true)
     @classroom = classroom
   end
 

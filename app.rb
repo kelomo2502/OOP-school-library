9a1ba564fff3fafd238f178251f4eb093b40cf58
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-08 23:46:01 +0100

    Add create rentals method

--- app.rb
+++ app.rb
@@ -77,4 +77,18 @@ class App
     puts " '#{new_book.title}' added successfully"
   end
 
+  def create_rental
+    puts 'Enter date'
+    date = gets.chomp
+    puts 'Select book number'
+    list_books
+    book = gets.chomp.to_i
+    puts 'Enter person by number NOT ID'
+    people = [*@students, *@teachers]
+    people.each_with_index { |s, i| print "(#{i}) [#{s.class}] Name: #{s.name}, ID: #{s.id}, Age: #{s.age}\n" }
+    person = gets.chomp.to_i
+    new_rental = Rental.new(date, @books[book], people[person])
+    @rentals << new_rental
+    puts 'Rental added successfully'
+  end
 end

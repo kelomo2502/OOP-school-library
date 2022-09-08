f55ec4bdadfa94157f6493d4e61ab1aeb7ed0b71
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-08 23:42:29 +0100

    Add list rentals method

--- app.rb
+++ app.rb
@@ -16,4 +16,14 @@ class App
   def list_books
     @books.each_with_index { |book, i| print "(#{i}) Title: \"#{book.title}\", Author: #{book.author}\n" }
   end
+
+  def list_rentals
+    puts 'Enter person ID: '
+    user_input = gets.chomp.to_i
+    user_rentals = []
+    @rentals.each { |r| user_input == r.person.id ? user_rentals << r : '' }
+    print "Rentals: \n"
+    user_rentals.each { |r| print " Date: #{r.date}, Title: #{r.book.title}, Author: #{r.book.author}\n" }
+  end
+
 end

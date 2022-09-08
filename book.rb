9fc51f2d6570334dcc8f14327a752afacdebc664
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-08 06:21:17 +0100

    fix linter errors

--- book.rb
+++ book.rb
@@ -1,11 +1,11 @@
-require './rental.rb'
+require './rental'
 
 class Book
   attr_accessor :title, :author, :rentals
+
   def initialize(title, author)
     @title = title
     @author = author
     @rentals = []
-
   end
 end

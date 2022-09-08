b2e2ea8fc04ebf029290491ffb7d4aa32d031d48
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-08 23:41:54 +0100

    Add list books method

--- app.rb
+++ app.rb
@@ -12,4 +12,8 @@ class App
     @teachers = []
     @rentals = []
   end
+
+  def list_books
+    @books.each_with_index { |book, i| print "(#{i}) Title: \"#{book.title}\", Author: #{book.author}\n" }
+  end
 end

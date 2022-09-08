493ed7ee352c74cc035cdee3971c3ee0afe9dc2d
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-08 23:45:06 +0100

    Add create book method

--- app.rb
+++ app.rb
@@ -66,4 +66,15 @@ class App
     end
   end
 
+  def create_book
+    print "Create book \n"
+    print ' Enter book title: '
+    title = gets.chomp
+    print ' Enter author name: '
+    author = gets.chomp
+    new_book = Book.new(title, author)
+    @books << new_book
+    puts " '#{new_book.title}' added successfully"
+  end
+
 end

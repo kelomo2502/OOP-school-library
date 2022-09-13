f9580868d32e1af3d71761cfdb646eead58da52c
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-13 06:32:33 +0100

    Add Create rental class

---
+++ create_rental.rb
@@ -0,0 +1,22 @@
+class create_rental
+  def initialize(rental, books, people)
+    @rental = rental
+    @books = books
+    @people = people
+  end
+
+  def create_rental
+    puts 'Enter date'
+    date = gets.chomp
+    puts 'Select book number'
+    list_books
+    book = gets.chomp.to_i
+    puts 'Enter person by number NOT ID'
+    people = [*@students, *@teachers]
+    people.each_with_index { |s, i| print "(#{i}) [#{s.class}] Name: #{s.name}, ID: #{s.id}, Age: #{s.age}\n" }
+    person = gets.chomp.to_i
+    new_rental = Rental.new(date, @books[book], people[person])
+    @rentals << new_rental
+    puts 'Rental added successfully'
+  end
+end

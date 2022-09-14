58a9b82cc4f36637deb793fe6e6c347a84cfe712
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-14 13:57:47 +0100

    Fix linters

--- interractions.rb
+++ interractions.rb
@@ -1,25 +1,29 @@
-class Interaction
-  def interaction(app)
+class Interraction
+  def interraction(app)
     user_input = options
     case user_input
     when 1
       app.list_books
-      interaction(app)
+      interraction(app)
     when 2
       app.list_people
-      interaction(app)
+      interraction(app)
     when 3
+      app.all_people
       CreatePerson.new(app.people).create_person
-      interaction(app)
+      interraction(app)
     when 4
+      app.all_books
       CreateBook.new(app.books).create_book
-      interaction(app)
+      interraction(app)
     when 5
+      app.all_books
+      app.all_people
       CreateRental.new(app.rentals, app.books, app.people).create_rental
-      interaction(app)
+      interraction(app)
     when 6
       app.list_rentals
-      interaction(app)
+      interraction(app)
     end
   end
 end

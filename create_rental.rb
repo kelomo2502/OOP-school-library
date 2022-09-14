8695dfcb1a4bb2bbb01d813281d2b8371cda7f26
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-09-14 13:57:07 +0100

    Ad JSon obect rentals to create rentals

--- create_rental.rb
+++ create_rental.rb
@@ -1,6 +1,6 @@
 class CreateRental
   def initialize(rental, books, people)
-    @rental = rental
+    @rentals = rental
     @books = books
     @people = people
   end
@@ -9,15 +9,22 @@ class CreateRental
     puts 'Enter date'
     date = gets.chomp
     puts 'Select book number'
-
-    @books.each_with_index { |book, i| print "(#{i}) Title: \"#{book.title}\", Author: #{book.author}\n" }
-
+    @books.each_with_index { |book, i| print "(#{i}) Title: \"#{book['Title']}\", Author: #{book['Author']}\n" }
     book = gets.chomp.to_i
     puts 'Enter person by number NOT ID'
-    @people.each_with_index { |s, i| print "(#{i}) [#{s.class}] Name: #{s.name}, ID: #{s.id}, Age: #{s.age}\n" }
+    @people.each_with_index do |s, i|
+      print "(#{i}) [#{s['Person']}] Name: #{s['Name']}, ID: #{s['ID']}, Age: #{s['Age']}\n"
+    end
     person = gets.chomp.to_i
-    new_rental = Rentals.new(date, @books[book], @people[person])
-    @rental << new_rental
+    new_rental = Rental.new(date, @books[book], @people[person])
+    rental_obj = { Title: new_rental.book['Title'], Author: new_rental.book['Author'], Date: new_rental.date,
+                   ID: new_rental.person['ID'] }
+    @rentals << rental_obj
+
+    File.open('rentals.json', 'w+') do |file|
+      rent = JSON.dump(@rentals)
+      file.write(rent)
+    end
     puts 'Rental added successfully'
   end
 end
